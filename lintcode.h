ee783a018696d9cd4b0c9ae4b87e5c3cb59a0fcc
10ca1h0st <1114802868@qq.com>
2018-06-02 19:06:19 +0800

    Add files via upload

--- lintcode.h
+++ lintcode.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 /**
 author:10ca1h0st
@@ -8,8 +9,49 @@ from:https://www.lintcode.com/problem/digit-counts/description
 **/
 //START
 int digitCounts(int k,int n);
-int digitCounts(int k,int n){
-    return 0;
+int digitCounts(int k,int n){ 
+    //n = v_i*10^max+v_(i-1)*10^(max-1)+...+v_0*10^0
+    int max = 0;
+    while(1){
+        if(n == 0){
+            max = 0;
+            break;
+        }
+        int res = n/pow(10,max);
+        if(res == 0){
+            max--;
+            break;
+        }
+        max++;
+    }
+    
+    if(max == 0){
+        //n为个位数
+        if(k > n){
+            return 0;
+        }else{
+            return 1;
+        } 
+    }
+    else{
+        //n不为个位数
+        int v_i = n/pow(10,max);
+        if(k < v_i){
+            if(k == 0){
+                return pow(10,max-1)*v_i*max+digitCounts(k,n-v_i*pow(10,max));
+            }
+            return pow(10,max)+pow(10,max-1)*v_i*max+digitCounts(k,n-v_i*pow(10,max));
+        }
+        else if(k == v_i){
+            if(k == 0){
+                return pow(10,max-1)*v_i*max+digitCounts(k,n-v_i*pow(10,max));
+            }
+            return (n-v_i*pow(10,max)+1)+pow(10,max-1)*v_i*max+digitCounts(k,n-v_i*pow(10,max));
+        }
+        else{
+            return pow(10,max-1)*v_i*max+digitCounts(k,n-v_i*pow(10,max));
+        }
+    }
 }
 //END
 
