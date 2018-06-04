47cf588086bba4ca4caaa1b0787db3ed15345812
Xujie Ren <34127865+Ren-rxj@users.noreply.github.com>
2018-06-05 01:05:44 +0800

    Create digitCounts.c
    Count the number of k's between 0 and n. k can be 0 - 9.

---
+++ digitCounts.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<math.h>
+
+    int digitCounts(int k, int n) {
+        // write your code here
+        int i=0;
+	    int j=0;
+	    int m,a;
+	    int arr[n];
+        for(;i<=n;i++){
+        arr[i]=i;
+//      printf("%d\n",arr[i]);
+        }
+      
+    	while(k==0){
+    		return n/10+1;
+		}
+		
+		for(i=0;i<=n;i++){
+			for(a=0;a<=(int)log10(n);a++){
+				m=arr[i]/pow(10,((int)log10(n)-a));
+//				printf("pf=%d,arr=%d\n",m,arr[i]);
+				if((m-k)%10==0){
+					j=j+1;
+					//printf("j2=%d\n",arr[i]);
+				}
+			}
+		}	
+		return j;
+    }
+
+int main(){
+	int k=0,n=19;
+	int c=digitCounts(k,n);
+	printf("%d\n",c);
+}
