2c81f30779faaba359fe29e5faf3ec1b14d24aab
Xujie Ren <34127865+Ren-rxj@users.noreply.github.com>
2018-06-12 00:21:09 +0800

    Update printListFromTailToHead.c

--- printListFromTailToHead.c
+++ printListFromTailToHead.c
@@ -1,3 +1,9 @@
+/*
+* AUTHOR:Ren Xujie
+* DATE:2018-6-11
+* CODE DESCRIPTION:PRINT A CHAIN TABLE REVERSELY
+*/
+
 /**
 *  struct ListNode {
 *        int val;
@@ -7,12 +13,13 @@
 *        }
 *  };
 */
+
 vector<int> printListFromTailToHead(ListNode* head) {
-    vector<int> value;
+    vector<int> value;      //定义一个存储输出的容器
     if(head!=NULL){
         value.insert(value.begin(),head->val);
         if(head->next!=NULL){
-            vector<int> arr=printListFromTailToHead(head->next);
+            vector<int> arr=printListFromTailToHead(head->next);        //采用递归调用的形式遍历链表直到表尾
             if(arr.size()>0)
                 value.insert(value.begin(),arr.begin(),arr.end());
         }
